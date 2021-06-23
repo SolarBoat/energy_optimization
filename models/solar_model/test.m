b49c52e548ec233db1bcea1e0d6c68516df64faf
LuggiStruggi <lukasmkoenig@gmx.net>
2021-06-23 17:08:35 +0200

    better test

--- models/solar_model/test.m
+++ models/solar_model/test.m
@@ -7,15 +7,21 @@ import casadi.*
 
 dt = datetime('now');
 
-lt_0 = dt.Hour + dt.Minute/60;
+ct = dt.Hour + dt.Minute/60;
 ordinal_day = day(dt,'dayofyear');
 
 
 time_zone = 1; % Germany UTC+1
-local_time = linspace(lt_0, lt_0+48, 500);
+local_time = linspace(0, 24, 500);
 latitude = 47.997791; % Freiburg coordinates
 longitude = 7.842609;
 
-di = arrayfun(@(t) di_full_model(ordinal_day, time_zone, t, latitude, longitude), local_time);
 
-plot(local_time, di)
+di = arrayfun(@(t) di_full_model(ordinal_day, time_zone, t, latitude, longitude), local_time);
+ct_di = di_full_model(ordinal_day, time_zone, ct, latitude, longitude);
+
+plot(local_time, di); hold on;
+plot(ct, ct_di, 'r.', 'Markersize', 15);
+legend('radiation curve', 'current radiation')
+xlabel('time [hours]')
+ylabel('direct radiation [kW/m^2]')

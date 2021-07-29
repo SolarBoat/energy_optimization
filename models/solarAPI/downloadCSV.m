58bb717400ad0159da9b3d70d82f785528a0f4eb
LuggiStruggi <lukasmkoenig@gmx.net>
2021-07-29 13:21:08 +0200

    api key now in seperate text file instead of online on github

--- models/solarAPI/downloadCSV.m
+++ models/solarAPI/downloadCSV.m
@@ -1,3 +1,5 @@
-data = webread('https://api.solcast.com.au/rooftop_sites/c1dd-f1ba-986d-dee5/forecasts?format=csv&api_key=tiQc74hB1dsEU2LvBVKxxB5LA4lQumVQ');
+key = fileread('api-key.txt');
+url = 'https://api.solcast.com.au/rooftop_sites/c1dd-f1ba-986d-dee5/forecasts?format=csv&api_key=';
+data = webread(join([url, key]));
 data = [data; data(end,:); data(end,:)];
-writetable(data,'data.csv');
+writetable(data,'data3.csv');
